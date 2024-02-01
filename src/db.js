6e87ce83b25640d589892f22cd7501713774a666
MDPereyra22 <pereyra.dmatias@gmail.com>
2024-02-01 13:17:10 -0300

    Actualiza DB

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dbfood_hi8l`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
